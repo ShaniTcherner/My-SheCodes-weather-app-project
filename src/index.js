e033b43f03d7f37aebcbf69f247a0888b9edcd59
ShaniTcherner <shantcherner@gmail.com>
2022-10-14 17:37:00 -0700

    editing the icon element

--- src/index.js
+++ src/index.js
@@ -149,4 +149,3 @@ iconElement.setAttribute(
   "src",
   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 );
-iconElement.setAttribute("alt", response.data.weather[0].description);
